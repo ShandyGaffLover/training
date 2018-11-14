727ae20db84def332edbe7349669c809db3a37b1
ShandyGaffLover <sd@anonymous.mail-box.ne.jp>
2018-11-14 22:38:40 +0900

    [update]ユーザー情報をCSVファイルから読み込むように修正

--- sample001/src/main/java/org/shandygafflover/sample001/UserDetailsService.java
+++ sample001/src/main/java/org/shandygafflover/sample001/UserDetailsService.java
@@ -1,6 +1,13 @@
 package org.shandygafflover.sample001;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,17 +15,28 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
 
+	@Autowired
+	ResourceLoader resourceLoader;
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-		String VALID_USERNAME = "username";
+		Resource usersCsv = resourceLoader.getResource("classpath:"+"users.csv");
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(usersCsv.getFile()));
+			String line;
+			while ((line = br.readLine()) != null) {
+				String[] userRow = line.split(",", 0);
+				if(StringUtils.equals(username,userRow[0])) {
+					return new User(userRow[0], userRow[1], AuthorityUtils.createAuthorityList(userRow[2]));
+				}
+			}
+			br.close();
 
-		if(StringUtils.equals(username, VALID_USERNAME)) {
-			return new User(username, "password", AuthorityUtils.createAuthorityList("USER"));
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-
 		throw new UsernameNotFoundException("Not Found: " + username);
-
 
 	}
 
