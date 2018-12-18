aabb8cd4f1595e8a802ec416118820c0c1683c19
ShandyGaffLover <sd@anonymous.mail-box.ne.jp>
2018-12-18 20:47:52 +0900

    [add]CodilityのLessonsの回答を追加
    100% Detected time complexity:O(N * log(N))

---
+++ Codility/Lessons/Lesson6_Sorting/MaxProductOfThree.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] A) {
+        Arrays.sort(A);
+        return Math.max(A[0]*A[1]*A[A.length-1], A[A.length-3]*A[A.length-2]*A[A.length-1]);
+    }
+}
