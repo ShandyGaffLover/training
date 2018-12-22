92915e332168eda182847154acece4bb56d5c84c
ShandyGaffLover <sd@anonymous.mail-box.ne.jp>
2018-12-22 14:11:44 +0900

    [update]再回答を試みる
    Correctness:87%; Performance:25%
    O(N**2)

--- Codility/Lessons/Lesson6_Sorting/NumberOfDiscIntersections.java
+++ Codility/Lessons/Lesson6_Sorting/NumberOfDiscIntersections.java
@@ -1,22 +1,46 @@
 import java.util.*;
 
 class Solution {
+    
+    static final int COL_CENTER=0;
+    static final int COL_EDGE=1;
+    static final int MAX_INTERSECTING_PAIRS = 10000000;
+    
     public int solution(int[] A) {
+
+        // 変換統治法：数直線と各円の交点を楔Wedgeとして保存する
+        int[][] wedges = createWedges(A);
+        
+        // Wedgeをその位置でソートする。Ref: https://teratail.com/questions/80128
+        Arrays.sort(wedges, (a, b) -> Integer.compare(a[COL_EDGE], b[COL_EDGE]));
+        
+        // 数直線をなぞり、同じ円由来のWedge間に存在するWedgeの数を加算する。
+        Map<Integer, Integer> map = new HashMap<>();
         int dest = 0;
-        for(int i=0; i<A.length; i++){
-            if(A[i]>=A.length){
-                dest += A.length-i;
+        for(int[] wedge: wedges){
+            if(!map.containsKey(wedge[COL_CENTER])){
+                map.put(wedge[COL_CENTER], wedge[COL_EDGE]);
                 continue;
             }
-            for(int j=i+1; j<A.length; j++){
-                if((A[i]+A[j])>=(j-i)){
-                    dest += 1;
-                }
+            // 自身を除外し、そのときまで通過したWedgeの数を加算する
+            map.remove(wedge[COL_CENTER]);
+            dest += map.size();
+
+            if(dest>MAX_INTERSECTING_PAIRS){
+                return -1;
             }
         }
-        if(dest > 10000000){
-            return -1;
-        }
         return dest;
+    }
+    
+    private int[][] createWedges(int[] A){
+        int[][] wedges = new int[2*A.length][2];
+        for(int i=0; i<A.length; i++){
+            wedges[i][COL_CENTER]=i;
+            wedges[i][COL_EDGE]=i-A[i];
+            wedges[i+A.length][COL_CENTER]=i;
+            wedges[i+A.length][COL_EDGE]=i+A[i];
+        }
+        return wedges;
     }
 }
